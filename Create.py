4e21cfdb7ca514f57b69eaccd1c1918cc7ad6fe9
Adrian Grebin <agrebin@technisys.com.ar>
2016-09-04 10:54:43 -0300

    Initial commit

---
+++ Create.py
@@ -0,0 +1,16 @@
+#/usr/bin/env python
+from  bitcoin import *
+from pybitcointools import *
+
+def main():
+    """
+
+    :rtype: basestring
+    """
+    import argparse
+    parser = argparse.ArgumentParser(description="Solution for fricko's 1st btc quest")
+    parser.add_argument()
+
+
+if __name__ == "__main__"
+    main()
